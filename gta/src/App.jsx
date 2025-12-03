1edfa3725b17668c4cdc828598e80b5253b9e2c1
Prakash K <prakash@HQ.CLI.IN>
2025-12-03 11:28:28 +0530

    Started Project

---
+++ gta/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <main>
+        <h1 class="text-3xl font-bold underline text-redpink">Hello world!</h1>
+      </main>
+    </>
+  );
+};
+
+export default App;
