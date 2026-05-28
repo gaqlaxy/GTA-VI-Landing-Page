058173a53cee961aae7878253944fc5e6921e59d
Prakash K <v4vripper@gmail.com>
2026-05-28 16:39:52 +0530

    Added Video

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ScrollTrigger } from "gsap/all";
 import HeroSection from "./sections/HeroSection";
 import Navbar from "./sections/Navbar";
 import Practice from "./sections/Practice";
+import FirstVideo from "./sections/FirstVideo";
 
 gsap.registerPlugin(ScrollTrigger);
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <main>
         <Navbar />
         <HeroSection />
+        <FirstVideo/>
         {/* <Practice /> */}
       </main>
     </>
