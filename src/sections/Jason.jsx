7970483af2df02ca7e4efd9b64052f1a9b23f405
Prakash K <v4vripper@gmail.com>
2026-07-06 17:13:39 +0530

    Working JasonDual

--- src/sections/Jason.jsx
+++ src/sections/Jason.jsx
@@ -8,6 +8,18 @@ const Jason = () => {
     <h1>
         Jason Duval
     </h1>
+    <h2>
+      Jason wants an easy life, but things just keep getting harder.
+    </h2>
+    <p>
+      Jason grew up around grifters and crooks. After a stint in the Army trying to shake off his troubled teens, he found himself in the Keys doing what he knows best, working for local drug runners. It might be time to try something new.
+    </p>
+    <div className="jason-2">
+      <img src="/images/jason-2.webp" alt="" />
+    </div>
+        </div>
+        <div className="space-y-5 mt-96 img-box">
+
         </div>
     </section>
     </>
